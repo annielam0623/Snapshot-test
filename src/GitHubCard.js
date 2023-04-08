393c0e4b96331ac2e665a8d902f2d23426de95c6
annielam0623 <annzhouyy@gmail.com>
2023-04-07 22:49:01 -0700

    finished

---
+++ src/GitHubCard.js
@@ -0,0 +1,9 @@
+import { Card } from "react-bootstrap";
+
+export default function GitHubCard() {
+    return <Card>
+        <h1>AL0623</h1>
+        <img src='./githubProfilePhoto.png' alt='github profile photo'/>
+        <p>HOW ARE YOU TODAY?</p>
+    </Card>
+}
